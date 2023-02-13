ba624da2f6d20f32c0509203ff98731b0114270c
Emmanuel Oyiboke <emmanueloyiboke@gmail.com>
2023-02-13 17:14:33 +0100

    simp change

--- src/components/Section3.js
+++ src/components/Section3.js
@@ -37,7 +37,7 @@ export default function Section3() {
             <Image width={221} height={69} src="/Google-Play.png" alt="" />
           </div>
         </div>
-        <Image width={854} height={640} className={styles.img4} src="/section3-img/img4.png" alt="" />
+        <Image width={854} height={640} priority className={styles.img4} src="/section3-img/img4.png" alt="" />
         
       </section>
     </>
